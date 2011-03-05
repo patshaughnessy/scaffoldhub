67af0a2f4f8979079dfd66b69fe96b84f63e4568
Pat Shaughnessy <pat@patshaughnessy.net>
2011-03-05 12:25:33 -0500

    removed spec_location.rb

--- lib/scaffoldhub.rb
+++ lib/scaffoldhub.rb
@@ -1,8 +1,7 @@
 require 'scaffoldhub/remote_file'
-require 'scaffoldhub/spec_location'
 require 'scaffoldhub/specification'
-require 'scaffoldhub/template_file'
 require 'scaffoldhub/scaffold_spec'
+require 'scaffoldhub/template_file'
 require 'scaffoldhub/helper'
 
 module Scaffoldhub
