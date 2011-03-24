2f83c0e99fae860d6ccdaea079a17058d3c785ae
Pat Shaughnessy <pat@patshaughnessy.net>
2011-03-24 13:00:44 -0400

    create_migration_file

--- lib/generators/active_record/scaffoldhub_generator.rb
+++ lib/generators/active_record/scaffoldhub_generator.rb
@@ -11,9 +11,19 @@ module ActiveRecord
     class_option :scaffold, :default => 'default', :banner => "SCAFFOLD_NAME",  :type => :string,  :desc => "Scaffold to use"
     class_option :local,    :default => false,     :banner => "LOCAL SCAFFOLD", :type => :boolean, :desc => "Use a local scaffold, not scaffoldhub.org"
 
+    class_option :migration,  :type => :boolean
+    class_option :timestamps, :type => :boolean
+    class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
+
     def create_model_file
       model_template = find_template_file('active_record', 'templates/model.rb')
       template model_template.src, File.join('app/models', class_path, "#{file_name}.rb") if model_template
+    end
+
+    def create_migration_file
+      return unless options[:migration] && options[:parent].nil?
+      migration_template = find_template_file('active_record', 'templates/migration.rb')
+      migration_template migration_template.src, "db/migrate/create_#{table_name}.rb" if migration_template
     end
 
   end
